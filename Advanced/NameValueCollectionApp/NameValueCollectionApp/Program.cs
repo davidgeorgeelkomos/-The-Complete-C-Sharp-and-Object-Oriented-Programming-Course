53702f01ecda3ce72b82ca4074d2be5e195bddbc
David George <david.g.elkomos@gmail.com>
2024-02-03 13:03:51 +0200

    Adding NameValueCollection file

---
+++ Advanced/NameValueCollectionApp/NameValueCollectionApp/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NameValueCoollectionApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Creating
+            NameValueCollection cities = new NameValueCollection();
+
+            //Adding
+            cities.Add("Germany", "Berlin");
+            cities.Add("Germany", "Hamburg");
+            cities.Add("Germany", "Frankfrut");
+            cities.Add("Italy", "Rome");
+            cities.Add("Italy", "Milan");
+
+            //Displaying
+            foreach (string item in cities)
+            {
+                Console.Write(item + ": ");
+                Console.WriteLine(cities[item]);
+            }
+            Console.WriteLine("-------------");
+
+            //Set
+            Console.WriteLine("After Chenging");
+            cities.Set("Italy", "Venice");
+            foreach (string item in cities)
+            {
+                Console.Write(item + ": ");
+                Console.WriteLine(cities[item]);
+            }
+            Console.WriteLine("-------------");
+
+            //Remove
+            cities.Remove("Germany");
+            foreach (string item in cities)
+            {
+                Console.Write(item + ": ");
+                Console.WriteLine(cities[item]);
+            }
+            Console.WriteLine("-------------");
+
+            //Contains
+            Console.WriteLine(cities.HasKeys());
+            Console.WriteLine("-------------");
+
+            //Remove all
+            cities.Clear();
+            Console.WriteLine(cities.HasKeys());
+            Console.WriteLine("-------------");
+        }
+    }
+}
