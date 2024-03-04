ee5e8a189dbc7c2852b432f26fcd05e1052a6a1a
David George <david.g.elkomos@gmail.com>
2024-03-05 01:10:12 +0200

    Adding file

---
+++ Advanced/Recursive Methods Exercise/Recursive Methods Exercise/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Recursive_Methods_Exercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = string.Empty;
+            path = @"C:\Users\a2aka\D\Games";
+            Console.WriteLine(path);
+            DisplayFolders(path, 0);
+        }
+        public static void DisplayFolders(string path,int indent)
+        {
+            foreach (var folder in Directory.GetDirectories(path))
+            {
+                Console.WriteLine($"{new string(' ',indent)} {Path.GetFileName(folder)}");
+                DisplayFolders(folder, indent);
+            }
+        }
+    }
+}
