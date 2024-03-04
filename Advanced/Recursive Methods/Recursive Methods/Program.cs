1359ae552184b83c28ed4249ec16851303320401
David George <david.g.elkomos@gmail.com>
2024-03-04 23:43:50 +0200

    Add Recursuve file

---
+++ Advanced/Recursive Methods/Recursive Methods/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recursive_Methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 4;
+            Console.WriteLine("Non recursive way");
+            Console.WriteLine(FactorialNonRecurive(number));
+
+            Console.WriteLine("Recursive way");
+            Console.WriteLine(Factorial(number));
+
+        }
+
+        //Non recurive way
+        public static int FactorialNonRecurive (int n)
+        {
+            if (n == 0)
+                return 1;
+            int value = 1;
+            for (int i = n; i > 0; i--)
+            {
+                value *= i;
+                Console.WriteLine(i);
+            }
+            return value;
+        }
+        //Recurive Way
+        public static int Factorial(int n)
+        {
+            if(n==0) 
+                return 1;
+            return n * Factorial(n - 1);
+
+        }
+    }
+}
