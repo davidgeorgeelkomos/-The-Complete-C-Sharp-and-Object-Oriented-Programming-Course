328a1d0a0249fbe1b078da1262a07e680324ef12
David George <david.g.elkomos@gmail.com>
2024-03-10 00:04:46 +0200

    adding files

---
+++ Advanced/Params Keyword/Params Keyword/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Params_Keyword
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LoopThroughArray(new string[4] {"John","Tramp","Obama","Biden"});
+            Console.WriteLine("--------------");
+            LoopThroughArray("Ahmad", "Mohamad", "Aly", "Aya");
+        }
+
+        public static void LoopThroughArray(params string[] names)
+        {
+            foreach (var name in names)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
