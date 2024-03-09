2120e70c3f1f64c09ad2022a73831f347b00811c
David George <david.g.elkomos@gmail.com>
2024-03-09 21:37:28 +0200

    adding file

---
+++ Advanced/Named Arguments/Named Arguments/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Named_Arguments
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DisplayPlayerInformaion(country: "Portugal", name: "Ronaldo", no: 7, goals:60);
+        }
+        public static void DisplayPlayerInformaion(int no, string name, int goals, string country = "Unknow")
+        {
+            Console.WriteLine("From the first method");
+            Console.WriteLine(no);
+            Console.WriteLine(name);
+            Console.WriteLine(goals);
+            Console.WriteLine(country);
+        }
+    }
+}
