65716218ccd8bd8d3ca344711bc29f6686dda467
David George <david.g.elkomos@gmail.com>
2024-03-09 23:13:40 +0200

    Adding file

---
+++ Advanced/Generic Arguments/Generic Arguments/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generic_Arguments
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DisplayInfo("John");
+            DisplayInfo(2);
+            DisplayInfo(2.5);
+
+            var PlayerInfo = (7, "Ronaldo", 60);
+            Console.WriteLine(PlayerInfo);
+        }
+        public static void DisplayInfo<T>(T info)
+        {
+            Console.WriteLine(info);
+        }
+    }
+}
