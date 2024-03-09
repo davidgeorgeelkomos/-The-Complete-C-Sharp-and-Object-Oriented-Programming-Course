bc26db5405fdca8d39fd9a396c11ba57b2f52580
David George <david.g.elkomos@gmail.com>
2024-03-09 21:22:05 +0200

    Adding folders

---
+++ Advanced/Optional Arguments/Optional Arguments/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Optional_Arguments
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DisplayPlayerInformaion(7, "Ronaldo", 60, "Portugal", "Alnassr");
+            Console.WriteLine("-----------");
+            DisplayPlayerInformaion(10, "Messi", 70);
+        }
+
+        //If i want to set a defult value i have to start from the last one
+        public static void DisplayPlayerInformaion(int no,string name,int goals,string country="Unknow",string club = "Unknow")
+        {
+            Console.WriteLine("From the first method");
+            Console.WriteLine(no);
+            Console.WriteLine(name);
+            Console.WriteLine(goals);
+            Console.WriteLine(country);
+            Console.WriteLine(club);
+        }
+        public static void DisplayPlayerInformaion(int no, string name, int goals)
+        {
+            Console.WriteLine("From the second method");
+            Console.WriteLine(no);
+            Console.WriteLine(name);
+            Console.WriteLine(goals);
+        }
+    }
+}
