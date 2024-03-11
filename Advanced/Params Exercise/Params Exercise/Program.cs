a678cd000077f2b2090738adaf363199e37fb179
David George <david.g.elkomos@gmail.com>
2024-03-11 12:49:34 +0200

    Adding the params folder

---
+++ Advanced/Params Exercise/Params Exercise/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Params_Exercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DisplayingPlayerInfo("Ahmad","adam");
+            DisplayingPlayerInfo(7);
+            DisplayingPlayerInfo(DateTime.Now);
+        }
+        public static void DisplayingPlayerInfo<T>(params T[] Info)
+        {
+            foreach (var name in Info)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
