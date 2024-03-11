4c1bb35f849c342eb2fde3ce3382971730bd765c
David George <david.g.elkomos@gmail.com>
2024-03-11 12:50:58 +0200

    Adding the Nasted folder

---
+++ Advanced/Nested Functions/Nested Functions/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nested_Functions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PerformMathOperations(1,2,3,4);
+            PerformMathOperations(1,2,3,4,5,6,7);
+        }
+        public static void PerformMathOperations(params int[] numbers)
+        {
+            Console.WriteLine("Add = " + AddingNumbers());
+            int AddingNumbers()
+            {
+                int result = 0;
+                foreach (var number in numbers)
+                {
+                    result = result + number;
+                }
+                return result;
+            }
+            Console.WriteLine("Multi = " + MultiNumbers());
+            int MultiNumbers()
+            {
+                int result = 1;
+                foreach (var number in numbers)
+                {
+                    result = result * number;
+                }
+                return result;
+            }
+            Console.WriteLine("------------");
+
+        }
+    }
+}
