d1af14495ece73615451bbf2a897d84a96ddce56
David George <david.g.elkomos@gmail.com>
2024-03-12 20:01:43 +0200

    Adding Folder

---
+++ Advanced/Exrension Method Exercise/Exrension Method Exercise/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exrension_Method_Exercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime Time = new DateTime(2000, 11, 1);
+            Console.WriteLine(Time);
+            Console.WriteLine("-----------");
+            Console.WriteLine(Time.TheDateTime());
+        }
+    }
+    static class NewDate
+    {
+        public static string TheDateTime(this DateTime date)
+        {
+            return string.Format("{0:dddd dd \nMMMM yyyy}",date);
+        }
+    }
+}
