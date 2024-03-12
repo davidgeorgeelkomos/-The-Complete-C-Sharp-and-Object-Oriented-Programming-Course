574ffded333460643007c2fec8158a7579ac7047
David George <david.g.elkomos@gmail.com>
2024-03-12 15:18:03 +0200

    Adding ExtenionMethod Folder

---
+++ Advanced/Extenion Methds/Extenion Methds/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extenion_Methds
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 10;
+            Console.WriteLine(x.IsGreater(20));
+
+            string text =  "aa";
+            Console.WriteLine(text.IsNumber());
+        }
+    }
+    static class MyCustomExtension
+    {
+        public static bool IsGreater(this int value,int number)
+        {
+            return value > number;
+        }
+        public static bool IsNumber(this string text)
+        {
+            return int.TryParse (text, out int result);
+        }
+    }
+}
