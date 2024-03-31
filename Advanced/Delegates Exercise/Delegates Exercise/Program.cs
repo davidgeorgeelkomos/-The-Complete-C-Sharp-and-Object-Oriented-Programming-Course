e3b6d64eb6b80510d70dd8d3a4fabebbb54360d8
David George <david.g.elkomos@gmail.com>
2024-04-01 00:50:19 +0200

    Addig Folders

---
+++ Advanced/Delegates Exercise/Delegates Exercise/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates_Exercise
+{
+    internal class Program
+    {
+        public delegate void PlayerNameGoals(Dictionary<string, int> Players);
+        static void Main(string[] args)
+        {
+            Dictionary<string,int> players = new Dictionary<string,int>();
+            players.Add("Messi", 60);
+            players.Add("Ronaldo", 59);
+            players.Add("Neymar", 58);
+
+            PlayerNameGoals player = new PlayerNameGoals(Displayer);
+            player(players);
+
+        }
+        public static void Displayer(Dictionary<string,int> Players)
+        {
+            foreach (var player in Players)
+            {
+                Console.WriteLine($"Player name is: {player.Key} \nAnd he scored: {player.Value}");
+                Console.WriteLine("---------------");
+            }
+        }
+    }
+}
