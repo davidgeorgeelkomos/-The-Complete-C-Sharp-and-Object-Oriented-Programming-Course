8e9c5cb5e06f3d818eb23e51eeaf28febff338ae
David George <david.g.elkomos@gmail.com>
2024-04-07 19:34:14 +0200

    Adding folder

---
+++ Advanced/F_W_A_M_L_E/F_W_A_M_L_E/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace F_W_A_M_L_E
+{
+    //F_W_A_M_L_E is short to("Func with anonymous method and lambda expression")
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "David George";
+
+            //Func
+            Func<string, string> ConvertToUpper = UpperCasestring;
+            Console.WriteLine("using Func ");
+            Console.WriteLine(ConvertToUpper(name));
+            Console.WriteLine("-----------");
+
+            //Anonymous Method
+            Func<string,string>  anonymousMetode = delegate(string myName) { return myName.ToUpper(); };
+            Console.WriteLine("using Anonymous Method:");
+            Console.WriteLine(anonymousMetode(name));
+            Console.WriteLine("-----------");
+
+            //Lambda expression
+            Func<string,string> lambdaExpression = text => text.ToUpper();
+            Console.WriteLine("using Lambda Expression");
+            Console.WriteLine(lambdaExpression(name));
+
+
+        }
+        public static string UpperCasestring(string inputstring)
+        {
+            return inputstring.ToUpper();
+        }
+    }
+}
