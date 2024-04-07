0fc73d5717423ec653a93da6ac498b987d310b24
David George <david.g.elkomos@gmail.com>
2024-04-07 15:08:47 +0200

    Adding folder

---
+++ Advanced/Generic Delegate_Action/Generic Delegate_Action/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generic_Delegate_Action
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Action<int> actionOne = DisplayInfo;
+            actionOne(09995000);
+
+            Action actionTwo = DisplayInfo;
+            actionTwo();
+        }
+        public static void DisplayInfo(int number)
+        {
+            Console.WriteLine("Daniel De Rossi number is : " + number);
+        }
+        public static void DisplayInfo()
+        {
+            Console.WriteLine("Gabrial Batista");
+        }
+    }
+}
