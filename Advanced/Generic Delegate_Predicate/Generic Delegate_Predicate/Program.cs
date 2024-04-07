9e9cd6e3afc5d7e3d991b3c41a8e912aa29bba6b
David George <david.g.elkomos@gmail.com>
2024-04-07 15:09:25 +0200

    Adding folder

---
+++ Advanced/Generic Delegate_Predicate/Generic Delegate_Predicate/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generic_Delegate_Predicate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Predicate<int> predicate = IsAdmin;
+            Console.WriteLine(predicate(10));
+        }
+        public static bool IsAdmin(int empNo)
+        {
+            if(empNo == 10)
+                return true;
+            else
+                return false;     
+        }
+    }
+}
