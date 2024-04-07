94e0d3f311b78e7d74661a72c8696f7e2d6f3d40
David George <david.g.elkomos@gmail.com>
2024-04-07 19:33:52 +0200

    Adding folder

---
+++ Advanced/A_W_A_M_L_E/A_W_A_M_L_E/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A_W_A_M_L_E
+{
+    //A_W_A_M_L_E is short to("Action with anonymous method and lambda expression")
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "David";
+            //Action
+            Action<string> actionMessage = ShowMeMessage;
+            Console.WriteLine("Using Action :");
+            actionMessage(name);
+            Console.WriteLine("---------");
+
+            //Anonymous Method
+            Action<string> anonymousMetode = delegate (string myName) { Console.WriteLine($"Hi, how are you today,{myName}?"); };
+            Console.WriteLine("Using Anonymous Method :");
+            anonymousMetode(name);
+            Console.WriteLine("----------");
+
+            //Lambda Expression
+            Action<string> anonymousExpression = text => Console.WriteLine($"Hi, how are you today,{text}?");
+            Console.WriteLine("Using Lambda Expression :");
+            anonymousExpression(name);
+
+
+        }
+        public static void ShowMeMessage(string name)
+        {
+            Console.WriteLine($"Hi, how are you today,{name}?");
+        }
+    }
+}
---
+++ Advanced/A_W_A_M_L_E/A_W_A_M_L_E/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("A_W_A_M_L_E")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("A_W_A_M_L_E")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ee2a4575-5889-415c-a30d-8ca6406c56cb")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
