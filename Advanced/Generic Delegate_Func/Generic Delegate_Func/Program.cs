4ee586dbec12f698651b43b6e0ce42ce4d4f48d0
David George <david.g.elkomos@gmail.com>
2024-04-07 15:07:57 +0200

    Adding folder

---
+++ Advanced/Generic Delegate_Func/Generic Delegate_Func/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generic_Delegate_Func
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<int, int, int> funcOne = AddingTwoNumbers;
+            Console.WriteLine(funcOne(7,8));
+            
+            Func<int> funcTwo = AddingTwoNumbers;
+            Console.WriteLine(funcTwo());
+        }
+        public static int AddingTwoNumbers(int x,int y)
+        {
+            return x+ y;
+        }
+        public static int AddingTwoNumbers()
+        {
+            int x = 5;
+            int y = 7;
+            int z;
+            z= x + y;
+            return z;
+        }
+    }
+}
