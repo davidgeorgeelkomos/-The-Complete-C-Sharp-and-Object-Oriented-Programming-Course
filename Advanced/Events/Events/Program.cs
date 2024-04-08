1061573575db32fa55c7ec778134c8abaa38fce1
David George <david.g.elkomos@gmail.com>
2024-04-08 23:48:52 +0200

    Adding folders

---
+++ Advanced/Events/Events/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Events
+{
+    public delegate void myDelegate(string name);
+    internal class Program
+    {
+        public event myDelegate myEvent;
+
+        public Program()
+        {
+            myEvent += new myDelegate(SayHiEnglish);
+            myEvent += new myDelegate(SayHiArabic);
+            myEvent += new myDelegate(SayHiItaly);
+        }
+        static void Main(string[] args)
+        {
+            Program prog = new Program();
+            prog.myEvent("jack");
+
+        }
+        public void SayHiEnglish(string name)
+        {
+            Console.WriteLine($"Hi {name}");
+        }
+        public void SayHiArabic(string name)
+        {
+            Console.WriteLine($"Marhaba {name}");
+        }
+        public void SayHiItaly(string name)
+        {
+            Console.WriteLine($"Caio {name}");
+        }
+    }
+}
