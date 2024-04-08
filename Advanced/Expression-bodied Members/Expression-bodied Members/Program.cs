5dfe2d8db28cab0932f5949783aa444c0bd8a7f4
David George <david.g.elkomos@gmail.com>
2024-04-09 01:02:37 +0200

    Adding folders

---
+++ Advanced/Expression-bodied Members/Expression-bodied Members/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Expression_bodied_Members
+{
+    internal class Program 
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(AddTwoNumberFaster(2,3));
+            Animal Dogs = new Animal();
+            Dogs.Name = "Lolo";
+            Dogs.Age = 2;
+            Console.WriteLine(Dogs.Name);
+        }
+        public static int AddTwoNumberFaster (int x,int y) => x + y;
+        public static int AddTwoNumbers(int x, int y) 
+        {
+            return x + y;
+        }
+    }
+    class Animal
+    {
+        public Animal() => Console.WriteLine("New animal has been created" );
+        private int age;
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+
+        private string name;
+        public string Name
+        {
+            get =>  name; 
+            set => name = value; 
+        }
+    }
+}
