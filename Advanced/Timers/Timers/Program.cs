9f3d08c919bb9316e4872195ef71ce042b64d967
David George <david.g.elkomos@gmail.com>
2024-04-09 01:01:55 +0200

    Adding folders

---
+++ Advanced/Timers/Timers/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace Timers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Timer timer = new Timer();
+            timer.Elapsed += OnEachSecond;
+            timer.Interval = 3000;
+            timer.Enabled = true;
+
+            Console.WriteLine("Press (X) to exit");
+            while (Console.Read() != 'x');
+            
+        }
+
+        private static void OnEachSecond(object sender, ElapsedEventArgs e)
+        {
+            Console.WriteLine("Yes,it is workind " +DateTime.Now.ToLongTimeString());
+        }
+    }
+}
