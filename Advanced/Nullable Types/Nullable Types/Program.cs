a458fa0940bfd81a4a0f3f9da1dc48572228716c
David George <david.g.elkomos@gmail.com>
2024-06-11 12:50:04 +0300

    Added

---
+++ Advanced/Nullable Types/Nullable Types/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nullable_Types
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime? myDate = null;
+            Console.WriteLine(myDate);
+
+            int? number = null;
+            Console.WriteLine(number);
+            //number = 5;
+
+            int numbertwo = 10;
+            Console.WriteLine(number ?? numbertwo);
+
+            string name = null;
+            name = "Dodo";
+
+            string nameTwo = "Ahmad";
+            Console.WriteLine(name ?? nameTwo);
+        }
+    }
+}
