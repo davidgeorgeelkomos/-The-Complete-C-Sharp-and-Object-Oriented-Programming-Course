8a0270988b21df5c938e9448295a9a509f4b7608
David George <david.g.elkomos@gmail.com>
2024-06-11 13:23:59 +0300

    Added

---
+++ Advanced/Conditional Operator/Conditional Operator/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conditional_Operator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (IsAdmin())
+            {
+                Console.WriteLine("Yes,he is an admin");
+            }
+            else
+            {
+                Console.WriteLine("No,he is not an admin");
+            }
+            Console.WriteLine("--------------");
+
+            Console.WriteLine(IsAdmin() ? "Is an Admin" : "Not admin" );
+        }
+        public static bool IsAdmin()
+        {
+            return true;
+        }
+    }
+}
