ca5c07dda99a351ac623ea9f568440fee1dbd9ce
David George <david.g.elkomos@gmail.com>
2024-06-11 13:57:44 +0300

    Added

---
+++ Advanced/Conditional Operator Exercise/Conditional Operator Exercise/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conditional_Operator_Exercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            Console.WriteLine(number %2 == 0 ? "Even number" : "Odd number");
+        }
+    }
+}
